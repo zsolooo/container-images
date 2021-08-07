79741a83855adce11ec322efcde75c3f4394661a
Zsolt Keseru <zsolooo@gmail.com>
2021-08-07 15:31:02 +0200

    Update ecr-credential-updater.sh

--- k8s-ecr-credential-updater/ecr-credential-updater.sh
+++ k8s-ecr-credential-updater/ecr-credential-updater.sh
@@ -15,10 +15,11 @@ SERVICE_ACCOUNT="${SERVICE_ACCOUNT:-default}"
 main() {
     # Temporary file for the secret
     ECR_SECRET_FILE=$(mktemp)
-    
+
     # Get credentials from AWS ECR API
+    log "Requesting ECR token with aws-cli..."
     request_credential > "${ECR_SECRET_FILE}"
-    
+
     # Update Kubernetes secrets in each target namespace
     for NS in ${TARGET_NAMESPACES}; do
         # Create or update Kubernetes secret
@@ -27,20 +28,20 @@ main() {
         # Manage the pullSecret for service account
         case "${SERVICE_ACCOUNT_ACTION}" in
             create)
-                create_service_account ;; 
+                create_service_account "${NS}" ;;
+                patch_service_account "${NS}" ;;
             patch)
-                patch_service_account ;;
+                patch_service_account "${NS}" ;;
             *)
-                log "No ServiceAccount action has been taken." ;; 
+                log "No ServiceAccount action has been taken." ;;
         esac
     done
 
     # Cleanup
-    rm -f "${ECR_SECRET_FILE}"
+    #rm -f "${ECR_SECRET_FILE}"
 }
 
 request_credential() {
-    log "Requesting ECR token with aws-cli..."
     aws ecr get-authorization-token \
     | jq '{"auths":{ (.authorizationData[0].proxyEndpoint) : {"auth": .authorizationData[0].authorizationToken}}}'
 }
@@ -50,19 +51,19 @@ apply_secret() {
 
     log "Applying secret in namespace ${NS}..."
     kubectl -n "${NS}" create secret generic \
-        --dry-run=true -o yaml \
+        --dry-run=client -o yaml \
         --type=kubernetes.io/dockerconfigjson \
         --from-file=.dockerconfigjson="${ECR_SECRET_FILE}" \
         "${ECR_CREDENTIALS_SECRETNAME}" \
     | kubectl -n "${NS}" apply -f -
 }
 
-patch_service_account() {
+create_service_account() {
     NS="${1}"
 
-    log "Patching service-account \"${SERVICE_ACCOUNT}\" in namespace ${NS}..."
-    kubectl -n "${NS}" patch serviceaccount "${SERVICE_ACCOUNT}" \
-        -p "{\"imagePullSecrets\": [{\"name\": \"${ECR_CREDENTIALS_SECRETNAME}\"}]}"
+    log "Create service-account \"${SERVICE_ACCOUNT}\" in namespace ${NS}..."
+    kubectl -n "${NS}" create serviceaccount --dry-run=client -o yaml "${SERVICE_ACCOUNT}" \
+    | kubectl apply -f -
 }
 
 patch_service_account() {
